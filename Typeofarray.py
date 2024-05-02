3d80f593d0117f817e3c0419ade2fd04701e24c4
Birbal Kumar <birbalkr1435@gmail.com>
2024-05-02 17:24:11 +0530

    Type_of_array

---
+++ Typeofarray.py
@@ -0,0 +1,27 @@
+
+# zero
+zero_array=np.zeros(4)
+zero_arr=np.zeros((3,4))
+print(zero_array)
+print("\n")
+print(zero_arr)
+
+print("\n")
+# ones
+one_array=np.ones(4)
+print(one_array)
+
+print("\n")
+# range
+range_arr=np.arange(4)
+print(range_arr)
+
+print("\n")
+# Diagonal
+one_array=np.eye(4)
+print(one_array)
+
+print("\n")
+# Linspace
+linespace_arr=np.linspace(1,2,num=5)
+print(linespace_arr)
